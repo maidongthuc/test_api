5a3b46ec0a4636db76425646e9b367e276592645
maidongthuc <chechanh2003@gmail.com>
2025-06-09 04:12:07 +0700

    cahtbot

--- src/retrieval.py
+++ src/retrieval.py
@@ -1,6 +1,6 @@
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
+from langchain.vectorstores.faiss import FAISS
 from src.file_loader import all_splits, all_splits_2
 
 os.environ["GOOGLE_API_KEY"] = "AIzaSyBr2cO37fWQbCifdkfCjD4sii21dfe1X88"
@@ -8,16 +8,16 @@ embedding_model = GoogleGenerativeAIEmbeddings(
     model="models/embedding-001"
 )
 
-chroma_db = Chroma.from_documents(all_splits, embedding=embedding_model)
-retriever = chroma_db.as_retriever(
+faiss_db = FAISS.from_documents(all_splits, embedding=embedding_model)
+retriever = faiss_db.as_retriever(
     search_type="similarity",
     search_kwargs={
         "k": 10
     }
 )
 
-chroma_db_2 = Chroma.from_documents(all_splits_2, embedding=embedding_model)
-retriever_2 = chroma_db_2.as_retriever(
+faiss_db_2 = FAISS.from_documents(all_splits_2, embedding=embedding_model)
+retriever_2 = faiss_db_2.as_retriever(
     search_type="similarity",
     search_kwargs={
         "k": 10
